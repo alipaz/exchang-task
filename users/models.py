1614407ddd4633ffc5957ee547191a6ae408437f
Ali <alipaz1381@gmail.com>
2023-10-13 23:46:45 +0330

    Feat:add new filed to base model

--- users/models.py
+++ users/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 
-# Create your models here.
+
+class UserBalance(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    balance = models.FloatField(default=0.0)
+
+    def __str__(self):
+        if self.user.first_name or self.user.last_name:
+            return self.user.first_name + ' ' + self.user.last_name
+        return self.user.username
