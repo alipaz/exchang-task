ea75a4332c0b3c9d8764ccb77a8bf8fa62768347
Ali <alipaz1381@gmail.com>
2023-10-13 23:53:43 +0330

    Feat:add new BuyCurrencyService

---
+++ exchange/services/order_services/buy_currency_service.py
@@ -0,0 +1,7 @@
+
+class BuyCurrencyService:
+
+    @staticmethod
+    def buy_from_exchange(currency_symbol, amount):
+        print(f"Buying {amount} units of {currency_symbol} from the exchange.")
+
